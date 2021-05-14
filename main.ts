3bb7ef6a2981d68eb1998efab6cd00618629f2c5
Isaiah123321 <84051071+Isaiah123321@users.noreply.github.com>
2021-05-14 15:35:15 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,103 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.destroy(effects.smiles, 500)
+    music.baDing.play()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.fire, 500)
+    music.zapped.play()
+})
+let ice_cream: Sprite = null
+let basketball: Sprite = null
+let qualifiedplayer: Sprite = null
+let age = game.askForNumber("state your age", 2)
+if (age >= 10) {
+    qualifiedplayer = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f f f f d d d d d e e f . . 
+        . f d d d d f 4 4 4 e e f . . . 
+        . f b b b b f 2 2 2 2 f 4 e . . 
+        . f b b b b f 2 2 2 2 f d 4 . . 
+        . . f c c f 4 5 5 4 4 f 4 4 . . 
+        . . . f f f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(qualifiedplayer)
+    qualifiedplayer.setStayInScreen(true)
+    info.setScore(0)
+    info.startCountdown(10)
+} else {
+    qualifiedplayer = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c c c c . . . 
+        . . . . . . c c 5 5 5 5 5 c . . 
+        . . . . . c 5 5 5 5 5 5 5 5 c . 
+        . . . . c b b b b b b 5 5 5 c . 
+        . . . . c 1 1 b b 1 b b c c . . 
+        . . . c 1 1 1 b b 1 1 1 c . . . 
+        . . . c 1 1 1 1 b 1 1 1 c . c c 
+        . . . c d 1 1 1 b 1 1 1 b b 5 c 
+        . . c c d 1 c 1 b 1 b 1 5 5 5 c 
+        . c c d d 1 1 1 1 1 b 1 b b 5 c 
+        f d d d 1 1 1 1 1 b b 1 f . c c 
+        f f f 1 1 1 1 1 1 b b b f . . . 
+        . . . f f 1 1 1 b b b 5 5 f . . 
+        . . . . . f f f 5 5 5 5 5 f . . 
+        . . . . . . . . f f f f f f . . 
+        `, SpriteKind.Player)
+}
+info.setLife(3)
+forever(function () {
+    scene.setBackgroundColor(1)
+})
+game.onUpdateInterval(500, function () {
+    basketball = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 4 4 4 4 . . . . . . . 
+        . . . . 4 4 f f 4 4 . . . . . . 
+        . . . 4 f 4 f f 4 f 4 . . . . . 
+        . . 4 4 4 f f f f 4 4 4 . . . . 
+        . . 4 f f f f f f f f 4 . . . . 
+        . . 4 f f f f f f f f 4 . . . . 
+        . . 4 4 4 f f f f 4 4 4 . . . . 
+        . . . 4 f 4 f f 4 f 4 . . . . . 
+        . . . . 4 4 f f 4 4 . . . . . . 
+        . . . . . 4 4 4 4 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, 50, 50)
+    ice_cream = sprites.createProjectileFromSide(img`
+        . . . . . 3 3 b 3 3 d d 3 3 . . 
+        . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
+        . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
+        . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
+        . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
+        . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
+        . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
+        . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
+        . 4 4 d 1 1 1 1 1 1 d d d b b . 
+        . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+        4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+        4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+        4 5 5 d 5 5 d b b b d d 3 . . . 
+        4 5 5 5 d d d d 4 4 b 3 . . . . 
+        . 4 5 5 5 4 4 4 . . . . . . . . 
+        . . 4 4 4 . . . . . . . . . . . 
+        `, 50, 50)
+    basketball.setPosition(randint(153, 0), 0)
+    basketball.setKind(SpriteKind.Enemy)
+    ice_cream.setPosition(randint(153, 0), 0)
+    ice_cream.setKind(SpriteKind.Food)
+})
